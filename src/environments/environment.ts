4ed24d533fcf86ebd5c88ecb13a58d3ac1caee5c
dlarson172 <dolars@microsoft.com>
2018-09-01 17:30:38 -0700

    9-1-2018

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  appId: '5ca3852926cdd0ac68d8b9d94a0195fa',
+  baseUrl: 'http://'
 };
 
 /*
